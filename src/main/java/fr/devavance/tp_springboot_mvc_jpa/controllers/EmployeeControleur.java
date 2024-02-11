6450d50430f24704fa3f8d829ec80f1f12b13a20
Gupta Varun Vedic <guptagivarun@gmail.com>
2024-02-11 21:23:24 +0100

    Add h2-database

--- src/main/java/fr/devavance/tp_springboot_mvc_jpa/controllers/EmployeeControleur.java
+++ src/main/java/fr/devavance/tp_springboot_mvc_jpa/controllers/EmployeeControleur.java
@@ -1,8 +1,9 @@
 package fr.devavance.tp_springboot_mvc_jpa.controllers;
 
 import com.sun.jdi.InterfaceType;
+
 import fr.devavance.tp_springboot_mvc_jpa.beans.Employee;
-import fr.devavance.tp_springboot_mvc_jpa.dao.IEmployeeDAO;
+import fr.devavance.tp_springboot_mvc_jpa.dao.IEmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,31 +14,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class EmployeeControleur {
 
-    private final IEmployeeDAO employeeDAO;
+    private final IEmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeControleur(IEmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeControleur(IEmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
 
 
     }
 
     @GetMapping("/employees")
     public String listEmployees(Model model) {
-        model.addAttribute("employees", employeeDAO.findAll());
+        model.addAttribute("employees", employeeRepository.findAll());
         return "employees";
     }
 
     @GetMapping("/")
     public String listEmployeesName(Model model) {
         model.addAttribute("employee",new Employee());
-        model.addAttribute("employees", employeeDAO.findAll());
+        model.addAttribute("employees", employeeRepository.findAll());
         return "liste_emp";
     }
 
     @PostMapping("/addemployee")
     public String addEmployee(Employee employee){
-        employeeDAO.add(employee);
+        employeeRepository.save(employee);
         return "redirect:/";
     }
 
